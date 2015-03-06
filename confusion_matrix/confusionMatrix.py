9a8f362c03abc3e1c86130babdf4becf6c41bdf3
yilin_guo <hduser@staff-01.(none)>
2015-03-06 14:54:12 +0800

    the before is wrong in display prob

--- confusion_matrix/confusionMatrix.py
+++ confusion_matrix/confusionMatrix.py
@@ -33,9 +33,10 @@ def plot_confusion_matrix(cm, title='Confusion Matrix', cmap = plt.cm.binary):
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
 cm = confusion_matrix(y_true, y_pred)
+print cm
 np.set_printoptions(precision=2)
-cm_normalized = cm.astype('float')/cm.sum(axis=0)[:, np.newaxis]
-
+cm_normalized = cm.astype('float')/cm.sum(axis=1)[:, np.newaxis]
+print cm_normalized
 plt.figure(figsize=(12,8), dpi=120)
 #set the fontsize of label.
 #for label in plt.gca().xaxis.get_ticklabels():
@@ -45,9 +46,9 @@ ind_array = np.arange(len(labels))
 x, y = np.meshgrid(ind_array, ind_array)
 
 for x_val, y_val in zip(x.flatten(), y.flatten()):
-    c = cm_normalized[x_val][y_val]
+    c = cm_normalized[y_val][x_val]
     if (c > 0.01):
-	plt.text(x_val, y_val, "%0.2f" %(c,), fontsize=7, va='center', ha='center')
+	plt.text(x_val, y_val, "%0.2f" %(c,), color='red', fontsize=7, va='center', ha='center')
 #offset the tick
 plt.gca().set_xticks(tick_marks, minor=True)
 plt.gca().set_yticks(tick_marks, minor=True)
@@ -55,6 +56,7 @@ plt.gca().xaxis.set_ticks_position('none')
 plt.gca().yaxis.set_ticks_position('none')
 plt.grid(True, which='minor', linestyle='-')
 plt.gcf().subplots_adjust(bottom=0.15)
+
 plot_confusion_matrix(cm_normalized, title='Normalized confusion matrix')
 #show confusion matrix
 plt.show()
